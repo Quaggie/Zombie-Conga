a9346383a342d5e516ea5f6ec6111bdc83ece862
Quaggie <jonathanpbijos@gmail.com>
2017-07-07 16:12:19 -0300

    Animations

--- Zombie Conga/GameScene.swift
+++ Zombie Conga/GameScene.swift
@@ -12,6 +12,7 @@ import GameplayKit
 class GameScene: SKScene {
     let background = SKSpriteNode(imageNamed: "background1")
     let zombie = SKSpriteNode(imageNamed: "zombie1")
+    let zombieAnimation: SKAction
     let playableRect: CGRect
     
     var lastUpdateTime: TimeInterval = 0
@@ -20,12 +21,23 @@ class GameScene: SKScene {
     
     let zombieMovePointsPerSec: CGFloat = 480.0
     var velocity: CGPoint = .zero
+    let zombieRotateRadiansPerSec: CGFloat = 4.0 * π
     
     override init(size: CGSize) {
         let maxAspectRatio:CGFloat = 16.0 / 9.0
         let playableHeight = size.width / maxAspectRatio
         let playableMargin = (size.height-playableHeight) / 2.0
         playableRect = CGRect(x: 0, y: playableMargin, width: size.width, height: playableHeight)
+        
+        var textures: [SKTexture] = []
+        for i in 1...4 {
+            textures.append(SKTexture(imageNamed: "zombie\(i)"))
+        }
+        textures.append(textures[2])
+        textures.append(textures[1])
+        zombieAnimation = SKAction.repeatForever(
+            SKAction.animate(with: textures, timePerFrame: 0.1))
+        
         super.init(size: size)
     }
     required init(coder aDecoder: NSCoder) {
@@ -50,11 +62,17 @@ class GameScene: SKScene {
         zombie.position = CGPoint(x: 400, y: 400)
         addChild(zombie)
         
+        run(SKAction.repeatForever(
+            SKAction.sequence([SKAction.run(spawnEnemy),
+                               SKAction.wait(forDuration: 2.0)])))
+        
+        run(SKAction.repeatForever(
+            SKAction.sequence([SKAction.run(spawnCat),
+                               SKAction.wait(forDuration: 1.0)])))
         debugDrawPlayableArea()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
         if let touch = touches.first {
             let touchLocation = touch.location(in: self)
             lastTouchLocation = touchLocation
@@ -91,24 +109,25 @@ class GameScene: SKScene {
             if distance <= (CGFloat(dt) * zombieMovePointsPerSec) {
                 zombie.position = lastTouchLocation
                 velocity = .zero
+                stopZombieAnimation()
             } else {
                 moveSprite(zombie, velocity: velocity)
-                rotateSprite(zombie, direction: velocity)
+                rotateSprite(zombie, direction: velocity, rotateRadiansPerSec: zombieRotateRadiansPerSec)
             }
         } else {
             moveSprite(zombie, velocity: velocity)
-            rotateSprite(zombie, direction: velocity)
+            rotateSprite(zombie, direction: velocity, rotateRadiansPerSec: zombieRotateRadiansPerSec)
         }
         boundsCheckZombie()
     }
     
     func moveSprite(_ sprite: SKSpriteNode, velocity: CGPoint) {
         let amountToMove = velocity * CGFloat(dt)
-        print("Amount to move: \(amountToMove)")
         sprite.position += amountToMove
     }
     
     func moveZombieToward(location: CGPoint) {
+        startZombieAnimation()
         let offset = location - zombie.position
         let length = offset.length()
         let direction = offset / length
@@ -142,8 +161,62 @@ class GameScene: SKScene {
         }
     }
     
-    func rotateSprite(_ sprite: SKSpriteNode, direction: CGPoint) {
+    func rotateSprite(_ sprite: SKSpriteNode, direction: CGPoint, rotateRadiansPerSec: CGFloat) {
         sprite.zRotation = direction.angle
+    }
+    
+    func spawnEnemy() {
+        let enemy = SKSpriteNode(imageNamed: "enemy")
+        enemy.position = CGPoint(
+            x: size.width + enemy.size.width/2,
+            y: CGFloat.random(
+                min: playableRect.minY + enemy.size.height/2,
+                max: playableRect.maxY - enemy.size.height/2
+            )
+        )
+        addChild(enemy)
+        
+        let actionMove = SKAction.moveTo(x: -enemy.size.width/2, duration: 2.0)
+        let actionRemove = SKAction.removeFromParent()
+        enemy.run(SKAction.sequence([actionMove, actionRemove]))
+    }
+    
+    func startZombieAnimation() {
+        if zombie.action(forKey: "animation") == nil {
+            zombie.run(SKAction.repeatForever(zombieAnimation), withKey: "animation")
+        }
+    }
+    
+    func stopZombieAnimation() {
+        zombie.removeAction(forKey: "animation")
+    }
+    
+    func spawnCat() {
+        let cat = SKSpriteNode(imageNamed: "cat")
+        cat.position = CGPoint(
+            x: CGFloat.random(min: playableRect.minX, max: playableRect.maxX),
+            y: CGFloat.random(min: playableRect.minY, max: playableRect.maxY)
+        )
+        cat.setScale(0)
+        addChild(cat)
+        
+        let appear = SKAction.scale(to: 1.0, duration: 0.5)
+        
+        let leftWiggle = SKAction.rotate(byAngle: π / 8.0, duration: 0.5)
+        let rightWiggle = leftWiggle.reversed()
+        let fullWiggle = SKAction.sequence([leftWiggle, rightWiggle])
+        
+        let scaleUp = SKAction.scale(by: 1.2, duration: 0.25)
+        let scaleDown = scaleUp.reversed()
+        let fullScale = SKAction.sequence([scaleUp, scaleDown, scaleUp, scaleDown])
+        
+        let group = SKAction.group([fullScale, fullWiggle])
+        let groupWait = SKAction.repeat(group, count: 10)
+        
+        let disappear = SKAction.scale(to: 0, duration: 0.5)
+        let removeFromParent = SKAction.removeFromParent()
+        let actions = [appear, groupWait, disappear, removeFromParent]
+        cat.run(SKAction.sequence(actions))
     }
 }
 
--- Zombie Conga/MyUtils.swift
+++ Zombie Conga/MyUtils.swift
@@ -78,3 +78,37 @@ extension CGPoint {
         return atan2(y, x)
     }
 }
+
+let π = CGFloat.pi
+
+func shortestAngleBetween(angle1: CGFloat, angle2: CGFloat) -> CGFloat {
+    let twoπ = π * 2.0
+    var angle = (angle2 - angle1) % twoπ
+    if angle >= π {
+        angle = angle - twoπ
+    }
+    if angle <= -π {
+        angle = angle + twoπ
+    }
+    return angle
+}
+
+extension CGFloat {
+    static func %(left: CGFloat, right: CGFloat) -> CGFloat {
+        return left.truncatingRemainder(dividingBy: right)
+    }
+    
+    func sign() -> CGFloat {
+        return self >= 0.0 ? 1.0 : -1.0
+    }
+    
+    static func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / Float(UInt32.max))
+        
+    }
+    
+    static func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        assert(min < max)
+        return CGFloat.random() * (max - min) + min
+    }
+}
